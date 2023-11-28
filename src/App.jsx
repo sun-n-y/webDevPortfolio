3b9a5832b7613f5825290701b34cc653f83ac0fb
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-28 11:58:15 -0500

    add navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import Navbar from './components/Navbar';
+
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 export default App;
