7868f1bdddcd102aa9c0d81ec0f324423d1e8f8f
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-28 15:52:30 -0500

    add custom classes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
 const Hero = () => {
   return (
     <div className="bg-emerald-100 py-24">
-      <div className="mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8">
+      <div className="align-elements grid md:grid-cols-2 items-center gap-8">
         <article>
           <h1 className="text-7xl font-bold tracking-wider">I'm Sunny</h1>
           <p className="mt-4 text-3xl text-slate-700 capitalize tracking-wide">
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { links } from '../data';
 const Navbar = () => {
   return (
     <nav className="bg-emerald-100">
-      <div className="mx-auto max-w-7xl px-8 py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8 ">
+      <div className="align-elements py-4 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-8 ">
         <h2 className="text-3xl font-bold ">
           Web<span className="text-emerald-600">Dev</span>
         </h2>
