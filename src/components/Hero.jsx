a1eed538f3f279b83415d6cae59736b9a6491c12
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-28 12:37:48 -0500

    add hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,32 @@
+import heroImg from '../assets/hero.svg';
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+
+const Hero = () => {
+  return (
+    <div className="bg-emerald-100 py-24">
+      <div className="mx-auto max-w-7xl px-8 grid md:grid-cols-2 items-center gap-8">
+        <article>
+          <h1 className="text-7xl font-bold tracking-wider">I'm Sunny</h1>
+          <p className="mt-4 text-3xl text-slate-700 capitalize tracking-wide">
+            front-end developer
+          </p>
+          <p className="mt-2 text-lg text-slate-700 capitalize tracking-wide">
+            turning ideas into interactive reality
+          </p>
+          <div className="flex gap-x-4 mt-4">
+            <a href="https://github.com/sun-n-y">
+              <FaGithubSquare className="h-8 w-8 text-slate-500 hover:text-slate-300 duration-300" />
+            </a>
+            {/* <a href="https://www.linkedin.com/in/sunny-patel-skp/">
+              <FaLinkedin className="h-8 w-8 text-slate-500 hover:text-slate-300 duration-300" />
+            </a> */}
+          </div>
+        </article>
+        <article className="hidden md:block">
+          <img src={heroImg} className="h-80 lg:h-96" />
+        </article>
+      </div>
+    </div>
+  );
+};
+export default Hero;
