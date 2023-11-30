968e680b0e02ecf1151bdd804d06436c63fd29bc
sunny patel <sunnykpatel.skp@gmail.com>
2023-11-30 12:17:02 -0500

    add resume and footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,41 @@
 import { links } from '../data';
+import { FaGithubSquare, FaLinkedin } from 'react-icons/fa';
+import { IoDocumentTextSharp } from 'react-icons/io5';
 import { BsArrowUpSquareFill } from 'react-icons/bs';
+import resume from '../assets/Resume.pdf';
 
 const Footer = () => {
   return (
     <footer className="bg-emerald-100 md:py-8">
-      <div className="align-elements justify-end py-10 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-10">
+      <div className="align-elements justify-between gap-y-4 py-10 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-10">
+        <div className="flex gap-x-2">
+          <a
+            href="https://www.linkedin.com/in/sunny-patel-skp/"
+            target="_blank"
+            rel="noreferrer"
+            className="flex items-center "
+          >
+            <FaLinkedin className="text-2xl text-black-500 hover:text-gray-300 duration-300" />
+          </a>
+          <a
+            href="https://github.com/sun-n-y/webDevPortfolio"
+            target="_blank"
+            rel="noreferrer"
+            className="flex items-center gap-x-1"
+          >
+            <FaGithubSquare className="text-2xl text-black-500 hover:text-gray-300 duration-300" />
+            <span>portfolio code</span>
+          </a>
+          <a
+            href={resume}
+            target="_blank"
+            rel="noreferrer"
+            className="flex items-center gap-x-1 "
+          >
+            <IoDocumentTextSharp className="text-2xl text-black-500 hover:text-gray-300 duration-300" />
+            resume
+          </a>
+        </div>
         <div className="flex gap-x-3 justify-end">
           {links.map((link) => {
             const { id, href, text } = link;
