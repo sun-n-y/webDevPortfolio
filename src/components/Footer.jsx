c54a566ef8948649149e59febb5fd8acd80c1a26
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-01 13:12:22 -0500

    add footer styles for small screen

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,36 +7,36 @@ import resume from '../assets/Resume.pdf';
 const Footer = () => {
   return (
     <footer className="bg-emerald-100 md:py-8">
-      <div className="align-elements justify-between gap-y-4 py-10 flex flex-col sm:flex-row sm:gap-x-16 sm:items-center sm:py-10">
-        <div className="flex gap-x-2">
+      <div className="align-elements justify-between py-10 flex flex-col-2 sm:flex-row sm:gap-x-1 sm:items-center sm:py-10">
+        <div className="flex justify-between gap-x-1 gap-y-1 flex-col">
           <a
             href="https://www.linkedin.com/in/sunny-patel-skp/"
             target="_blank"
             rel="noreferrer"
-            className="flex items-center "
+            className="flex items-center"
           >
             <FaLinkedin className="text-2xl text-black-500 hover:text-gray-300 duration-300" />
-          </a>
-          <a
-            href="https://github.com/sun-n-y/webDevPortfolio"
-            target="_blank"
-            rel="noreferrer"
-            className="flex items-center gap-x-1"
-          >
-            <FaGithubSquare className="text-2xl text-black-500 hover:text-gray-300 duration-300" />
-            <span>portfolio code</span>
           </a>
           <a
             href={resume}
             target="_blank"
             rel="noreferrer"
-            className="flex items-center gap-x-1 "
+            className="flex items-center gap-x-1 text-lg "
           >
             <IoDocumentTextSharp className="text-2xl text-black-500 hover:text-gray-300 duration-300" />
-            resume
+            Resume
+          </a>{' '}
+          <a
+            href="https://github.com/sun-n-y/webDevPortfolio"
+            target="_blank"
+            rel="noreferrer"
+            className="flex items-center gap-x-1 text-lg"
+          >
+            <FaGithubSquare className="text-2xl text-black-500 hover:text-gray-300 duration-300" />
+            <span>Portfolio code</span>
           </a>
         </div>
-        <div className="flex gap-x-3 justify-end">
+        <div className="flex gap-x-3 gap-y-1  flex-col justify-end">
           {links.map((link) => {
             const { id, href, text } = link;
             return (
@@ -49,13 +49,13 @@ const Footer = () => {
               </a>
             );
           })}
-          <a
-            href="#home"
-            className="capitalize text-2xl tracking-wide hover:text-emerald-600 duration-300"
-          >
-            <BsArrowUpSquareFill className="h-full" />
-          </a>
         </div>
+        <a
+          href="#home"
+          className="capitalize text-3xl tracking-wide self-end hover:text-emerald-600 duration-300"
+        >
+          <BsArrowUpSquareFill className="h-full" />
+        </a>
       </div>
     </footer>
   );
