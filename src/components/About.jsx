432719a687340db0d81582abe7ff56020a43a35c
sunny patel <sunnykpatel.skp@gmail.com>
2023-12-02 13:07:47 -0500

    add dark mode colors to about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,35 +3,46 @@ import { FaChevronRight } from 'react-icons/fa';
 
 const About = () => {
   return (
-    <section className="bg-white py-14 md:py-16 lg:py-24" id="about">
+    <section
+      className="bg-white dark:bg-dark-grey duration-200 py-14 md:py-16 lg:py-24"
+      id="about"
+    >
       <div className="align-elements">
         <SectionTitle text={'about'} />
       </div>
       <div className="grid pt-12 gap-8 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 align-elements">
         <div className="div flex items-center  gap-2">
           <FaChevronRight className="text-emerald-500" />
-          <h4 className="font-bold">Phone:</h4>
-          <h4 className="">(808) 649 8220</h4>
+          <h4 className="font-bold dark:text-slate-200 duration-200">Phone:</h4>
+          <h4 className="dark:text-slate-400 duration-200">(808) 649 8220</h4>
         </div>
         <div className="div flex items-center  gap-2">
           <FaChevronRight className="text-emerald-500" />
-          <h4 className="font-bold">City:</h4>
-          <h4 className="">Dalton, Georgia</h4>
+          <h4 className="font-bold dark:text-slate-200 duration-200">City:</h4>
+          <h4 className="dark:text-slate-400 duration-200">Dalton, Georgia</h4>
         </div>
         <div className="div flex items-center  gap-2">
           <FaChevronRight className="text-emerald-500" />
-          <h4 className="font-bold">Email:</h4>
-          <h4 className="">sunnykpatel.skp@gmail.com</h4>
+          <h4 className="font-bold dark:text-slate-200 duration-200">Email:</h4>
+          <h4 className="dark:text-slate-400 duration-200">
+            sunnykpatel.skp@gmail.com
+          </h4>
         </div>
         <div className="div flex items-center  gap-2">
           <FaChevronRight className="text-emerald-500" />
-          <h4 className="font-bold">Nationality:</h4>
-          <h4 className="">U.S. Citizen</h4>
-        </div>{' '}
+          <h4 className="font-bold dark:text-slate-200 duration-200">
+            Nationality:
+          </h4>
+          <h4 className="dark:text-slate-400 duration-200">U.S. Citizen</h4>
+        </div>
         <div className="div flex items-center  gap-2">
           <FaChevronRight className="text-emerald-500" />
-          <h4 className="font-bold">Hobbies:</h4>
-          <h4 className="">Sports, Travel, Meditation</h4>
+          <h4 className="font-bold dark:text-slate-200 duration-200">
+            Hobbies:
+          </h4>
+          <h4 className="dark:text-slate-400 duration-200">
+            Sports, Travel, Meditation
+          </h4>
         </div>
       </div>
     </section>
